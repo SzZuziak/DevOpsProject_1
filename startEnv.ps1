6c9cfeba5515311e448fd82da59f9e5b9b2c2d45
SzZuziak <szymonzuziak@gmail.com>
2021-05-15 23:17:48 +0200

    Variable for scriptblock job fixed

--- startEnv.ps1
+++ startEnv.ps1
@@ -1,9 +1,13 @@
-$wait = 60
+$wait = 20
+$pattern = "registry-(?!proxy)[a-z0-9]{5}"
+$registryConf = "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:host.docker.internal:5000"
+
 minikube start --driver=hyperv
 Start-sleep -s $wait
 
-Start-Job -ScriptBlock {kubectl port-forward --namespace kube-system 5000:5000}
+$registryPodName = kubectl get pod -n kube-system | Select-String -Pattern $pattern  | foreach {$_.matches} | select value
+Start-Job -ScriptBlock {kubectl port-forward $Using:registryPodName.Value --namespace kube-system 5000:5000}
 
-Start-sleep -s 20
-docker run --rm -d --network=host -v /mnt/registry:/var/lib/registry alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:host.docker.internal:5000"
+Start-sleep -s $wait
+docker run --rm -d --network=host -v /mnt/registry:/var/lib/registry alpine ash -c $registryConf
 Start-sleep -s $wait
