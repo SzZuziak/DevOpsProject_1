4b45ab54080411516f6b8a2ec10a2124ea071ba9
SzZuziak <szymonzuziak@gmail.com>
2021-05-15 20:28:16 +0200

    wait change from 120 -> 60 s

--- startEnv.ps1
+++ startEnv.ps1
@@ -1,8 +1,9 @@
+$wait = 60
 minikube start
-Start-sleep -s 120
+Start-sleep -s $wait
 
 Start-Job -ScriptBlock {kubectl port-forward --namespace kube-system 5000:5000}
 
 Start-sleep -s 20
 docker run --rm -d --network=host -v /mnt/registry:/var/lib/registry alpine ash -c "apk add socat && socat TCP-LISTEN:5000,reuseaddr,fork TCP:host.docker.internal:5000"
-Start-sleep -s 120
+Start-sleep -s $wait
